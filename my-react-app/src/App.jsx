4ba545b868750b9f356f82c44cff46b2689ef796
Poomsanthong <thanapoom.santhong10@gmail.com>
2025-10-15 21:51:05 +0200

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+const App = () => {
+  return (
+    <div>
+      <main>
+        <NavBar />
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ my-react-app/src/components/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+  { label: "Mac" },
+  { label: "iPad" },
+  { label: "iPhone" },
+  { label: "watch" },
+  { label: "AirPods" },
+  { label: "Vision" },
+];
